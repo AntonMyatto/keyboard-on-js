f27ed97454b9107d5360cf234a0a06e07b55d190
AntonMyatto <48759290+AntonMyatto@users.noreply.github.com>
2020-07-02 17:50:56 +0300

    first

---
+++ js/keyboard.js
@@ -0,0 +1,174 @@
+let firstBtn = document.querySelector('.one');
+let secondBtn = document.querySelector('.two');
+let thirdBtn = document.querySelector('.three');
+let fourBtn = document.querySelector('.four');
+let fiveBtn = document.querySelector('.five');
+let sixBtn = document.querySelector('.six');
+let sevenBtn = document.querySelector('.seven');
+let eightBtn = document.querySelector('.eight');
+let nineBtn = document.querySelector('.nine');
+let tenBtn = document.querySelector('.ten');
+
+let aBtn = document.querySelector('.A');
+let bBtn = document.querySelector('.B');
+let cBtn = document.querySelector('.C');
+let dBtn = document.querySelector('.D');
+let eBtn = document.querySelector('.E');
+let fBtn = document.querySelector('.F');
+let gBtn = document.querySelector('.G');
+let hBtn = document.querySelector('.H');
+let iBtn = document.querySelector('.I');
+let jBtn = document.querySelector('.J');
+
+let kBtn = document.querySelector('.K');
+let lBtn = document.querySelector('.L');
+let mBtn = document.querySelector('.M');
+let nBtn = document.querySelector('.N');
+let oBtn = document.querySelector('.O');
+let pBtn = document.querySelector('.P');
+let qBtn = document.querySelector('.Q');
+let rBtn = document.querySelector('.R');
+let sBtn = document.querySelector('.S');
+let tBtn = document.querySelector('.T');
+
+let uBtn = document.querySelector('.U');
+let vBtn = document.querySelector('.V');
+let wBtn = document.querySelector('.W');
+let xBtn = document.querySelector('.X');
+let yBtn = document.querySelector('.Y');
+let zBtn = document.querySelector('.Z');
+let skobleftBtn = document.querySelector('.skobleft');
+let skobrightBtn = document.querySelector('.skobright');
+let kavBtn = document.querySelector('.kav');
+
+let display = document.querySelector('.display');
+
+firstBtn.onclick = function(){
+    display.value+=1;
+};
+secondBtn.onclick = function(){
+    display.value+=2;
+  };
+  thirdBtn.onclick = function(){
+    display.value+=3;
+  };
+  fourBtn.onclick = function(){
+    display.value+=4;
+  };
+  fiveBtn.onclick = function(){
+    display.value+=5;
+  };
+  sixBtn.onclick = function(){
+    display.value+=6;
+  };
+  sevenBtn.onclick = function(){
+    display.value+=7;
+  };
+  eightBtn.onclick = function(){
+    display.value+=8;
+  };
+  nineBtn.onclick = function(){
+    display.value+=9;
+  };
+  tenBtn.onclick = function(){
+    display.value+=0;
+  };
+
+  //WORDS first
+
+  aBtn.onclick = function(){
+    display.value+='a';
+};
+   bBtn.onclick = function(){
+    display.value+='b';
+  };
+  cBtn.onclick = function(){
+    display.value+='c';
+  };
+  dBtn.onclick = function(){
+    display.value+='d';
+  };
+  eBtn.onclick = function(){
+    display.value+='e';
+  };
+  fBtn.onclick = function(){
+    display.value+='f';
+  };
+  gBtn.onclick = function(){
+    display.value+='g';
+  };
+  hBtn.onclick = function(){
+    display.value+='h';
+  };
+  iBtn.onclick = function(){
+    display.value+='i';
+  };
+  jBtn.onclick = function(){
+    display.value+='j';
+  };
+
+  //second words
+
+  kBtn.onclick = function(){
+    display.value+='k';
+};
+   lBtn.onclick = function(){
+    display.value+='l';
+  };
+  mBtn.onclick = function(){
+    display.value+='m';
+  };
+  nBtn.onclick = function(){
+    display.value+='n';
+  };
+  oBtn.onclick = function(){
+    display.value+='o';
+  };
+  pBtn.onclick = function(){
+    display.value+='p';
+  };
+  qBtn.onclick = function(){
+    display.value+='q';
+  };
+  rBtn.onclick = function(){
+    display.value+='r';
+  };
+  sBtn.onclick = function(){
+    display.value+='s';
+  };
+  tBtn.onclick = function(){
+    display.value+='t';
+  };
+
+  //third buttons
+
+  uBtn.onclick = function(){
+    display.value+='u';
+};
+   vBtn.onclick = function(){
+    display.value+='v';
+  };
+  wBtn.onclick = function(){
+    display.value+='w';
+  };
+  xBtn.onclick = function(){
+    display.value+='x';
+  };
+  yBtn.onclick = function(){
+    display.value+='y';
+  };
+  zBtn.onclick = function(){
+    display.value+='z';
+  };
+  
+  skobleftBtn.onclick = function(){
+    display.value+='(';
+  };
+  skobrightBtn.onclick = function(){
+    display.value+=')';
+  };
+  kavBtn.onclick = function(){
+    display.clear;
+  };
+
+
